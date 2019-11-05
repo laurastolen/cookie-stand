7a07bf485d63ed6e22263e6242f1ef2d83e0d7a8
laura antonius <laurastolen@gmail.com>
2019-11-05 11:23:00 -0800

    all locations functioning via literals

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
 // below is my revised code for the first day using object literals (only completed for seattle and tokyo):
 const OPERATINGHOURS = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+// SEATTLE
 var seattleLoc = {
   locName: 'Seattle',
   minHourlyCust: 23,
@@ -37,6 +38,7 @@ var SeattleTotalLi = document.createElement('li');
 SeattleTotalLi.textContent = `Total: ${seattleLoc.dailyTotalCookies} cookies`;
 seattleDiv.append(SeattleTotalLi);
 
+// TOKYO
 var tokyoLoc = {
   locName: 'Tokyo',
   minHourlyCust: 3,
@@ -67,87 +69,100 @@ var tokyoTotalLi = document.createElement('li');
 tokyoTotalLi.textContent = `Total: ${tokyoLoc.dailyTotalCookies} cookies`;
 tokyoDiv.append(tokyoTotalLi);
 
-
-
-
-
-
-
-
-
-/* below is my original code from during lab:
-var locations = [seattle, tokyo, dubai, paris, lima];
-
-var seattle = {
-  name: 'Seattle',
-  minHourlyCustomers: 23,
-  maxHourlyCustomers: 65,
-  avgCookiesPerSale: 6.3,
-  randomHourlyCustomers: function (min, max) {
-    min = this.minHourlyCustomers;
-    max = this.maxHourlyCustomers;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+// DUBAI
+var dubaiLoc = {
+  locName: 'Dubai',
+  minHourlyCust: 11,
+  maxHourlyCust: 38,
+  avgSale: 3.7,
+  randHourlyCust: function (min, max) {
+    min = this.minHourlyCust;
+    max = this.maxHourlyCust;
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
   },
-  totalCookiesPerHourArray: [],
-  totalDailyCookies: 0
+  purchasesByHour: [],
+  dailyTotalCookies: 0,
+};
+
+for (var hourIndex = 0; hourIndex < OPERATINGHOURS.length; hourIndex++) {
+  dubaiLoc.purchasesByHour.push(Math.ceil(dubaiLoc.avgSale * dubaiLoc.randHourlyCust()));
+  dubaiLoc.dailyTotalCookies += dubaiLoc.purchasesByHour[hourIndex];
 }
 
-var tokyo = {
-  name: 'tokyo',
-  minHourlyCustomers: 3,
-  maxHourlyCustomers: 24,
-  avgCookiesPerSale: 1.2,
-  randomHourlyCustomers: function (min, max) {
-    min = this.minHourlyCustomers;
-    max = this.maxHourlyCustomers;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  },
-  totalCookiesPerHourArray: [],
-  totalDailyCookies: 0
-
+var dubaiDiv = document.getElementById('dubai');
+for (var dubaiLi = 0; dubaiLi < OPERATINGHOURS.length; dubaiLi++) {
+  var dubaiHourlyLi = document.createElement('li');
+  dubaiHourlyLi.textContent = `${OPERATINGHOURS[dubaiLi]}: ${dubaiLoc.purchasesByHour[dubaiLi]} cookies`;
+  dubaiDiv.append(dubaiHourlyLi);
 }
 
-var dubai = {
-  name: 'dubai',
-  minHourlyCustomers: 11,
-  maxHourlyCustomers: 38,
-  avgCookiesPerSale: 3.7,
-  randomHourlyCustomers: function (min, max) {
-    min = this.minHourlyCustomers;
-    max = this.maxHourlyCustomers;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+var dubaiTotalLi = document.createElement('li');
+dubaiTotalLi.textContent = `Total: ${dubaiLoc.dailyTotalCookies} cookies`;
+dubaiDiv.append(dubaiTotalLi);
+
+// PARIS
+var parisLoc = {
+  locName: 'Paris',
+  minHourlyCust: 20,
+  maxHourlyCust: 38,
+  avgSale: 2.3,
+  randHourlyCust: function (min, max) {
+    min = this.minHourlyCust;
+    max = this.maxHourlyCust;
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
   },
-  totalCookiesPerHourArray: [],
-  totalDailyCookies: 0
+  purchasesByHour: [],
+  dailyTotalCookies: 0,
+};
+
+for (var hourIndex = 0; hourIndex < OPERATINGHOURS.length; hourIndex++) {
+  parisLoc.purchasesByHour.push(Math.ceil(parisLoc.avgSale * parisLoc.randHourlyCust()));
+  parisLoc.dailyTotalCookies += parisLoc.purchasesByHour[hourIndex];
 }
 
-var paris = {
-  name: 'paris',
-  minHourlyCustomers: 20,
-  maxHourlyCustomers: 38,
-  avgCookiesPerSale: 2.3,
-  randomHourlyCustomers: function (min, max) {
-    min = this.minHourlyCustomers;
-    max = this.maxHourlyCustomers;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  },
-  totalCookiesPerHourArray: [],
-  totalDailyCookies: 0
+var parisDiv = document.getElementById('paris');
+for (var parisLi = 0; parisLi < OPERATINGHOURS.length; parisLi++) {
+  var parisHourlyLi = document.createElement('li');
+  parisHourlyLi.textContent = `${OPERATINGHOURS[parisLi]}: ${parisLoc.purchasesByHour[parisLi]} cookies`;
+  parisDiv.append(parisHourlyLi);
 }
 
-var lima = {
-  name: 'lima',
-  minHourlyCustomers: 2,
-  maxHourlyCustomers: 16,
-  avgCookiesPerSale: 4.6,
-  randomHourlyCustomers: function (min, max) {
-    min = this.minHourlyCustomers;
-    max = this.maxHourlyCustomers;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+var parisTotalLi = document.createElement('li');
+parisTotalLi.textContent = `Total: ${parisLoc.dailyTotalCookies} cookies`;
+parisDiv.append(parisTotalLi);
+
+// LIMA NOT DONE YET!
+var limaLoc = {
+  locName: 'Lima',
+  minHourlyCust: 16,
+  maxHourlyCust: 4.6,
+  avgSale: 3.7,
+  randHourlyCust: function (min, max) {
+    min = this.minHourlyCust;
+    max = this.maxHourlyCust;
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
   },
-  totalCookiesPerHourArray: [],
-  totalDailyCookies: 0
+  purchasesByHour: [],
+  dailyTotalCookies: 0,
+};
+
+for (var hourIndex = 0; hourIndex < OPERATINGHOURS.length; hourIndex++) {
+  limaLoc.purchasesByHour.push(Math.ceil(limaLoc.avgSale * limaLoc.randHourlyCust()));
+  limaLoc.dailyTotalCookies += limaLoc.purchasesByHour[hourIndex];
 }
+
+var limaDiv = document.getElementById('lima');
+
+for (var limaLi = 0; limaLi < OPERATINGHOURS.length; limaLi++) {
+  var limaHourlyLi = document.createElement('li');
+  limaHourlyLi.textContent = `${OPERATINGHOURS[limaLi]}: ${limaLoc.purchasesByHour[limaLi]} cookies`;
+  limaDiv.append(limaHourlyLi);
+}
+
+var limaTotalLi = document.createElement('li');
+limaTotalLi.textContent = `Total: ${limaLoc.dailyTotalCookies} cookies`;
+limaDiv.append(limaTotalLi);
+
 
 
 // for each location: (ie, loop through the locations array?)
@@ -157,116 +172,9 @@ var lima = {
 //   store all those hourlytotals in another array
 //   display the hourlytotals array, each index in a <li>
 
-for (var hour = 0; hour < operatingHours.length; hour++) {
-  var hourlyTotalCookies = seattle.avgCookiesPerSale * seattle.randomHourlyCustomers();
-  hourlyTotalCookies = Math.ceil(hourlyTotalCookies);
-  seattle.totalCookiesPerHourArray.push(`${operatingHours[hour]}: ${hourlyTotalCookies}`);
-  seattle.totalDailyCookies += hourlyTotalCookies;
-}
 
 // to put the text in <li>s
 // 1 get a reference in the dom
 // 2 create elements we need to append to the dom
 // 3 set their contents
 // 4 add your elements to the dom
-
-var seattleDiv = document.getElementById('seattle');
-for (var i = 0; i < seattle.totalCookiesPerHourArray.length; i++) {
-  var hourlyLi = document.createElement('li');
-  hourlyLi.textContent = seattle.totalCookiesPerHourArray[i];
-  seattleDiv.append(hourlyLi);
-}
-var totalSeattleCookies = document.createElement('li');
-totalSeattleCookies.textContent = `Total: ${seattle.totalDailyCookies}`;
-seattleDiv.append(totalSeattleCookies);
-
-
-
-
-// tokyo below:
-// create array
-for (var hour = 0; hour < operatingHours.length; hour++) {
-  var hourlyTotalCookies = tokyo.avgCookiesPerSale * tokyo.randomHourlyCustomers();
-  hourlyTotalCookies = Math.ceil(hourlyTotalCookies);
-  tokyo.totalCookiesPerHourArray.push(`${operatingHours[hour]}: ${hourlyTotalCookies}`);
-  tokyo.totalDailyCookies += hourlyTotalCookies;
-}
-// create <li>s via dom
-var tokyoDiv = document.getElementById('tokyo');
-for (var i = 0; i < tokyo.totalCookiesPerHourArray.length; i++) {
-  var hourlyLi = document.createElement('li');
-  hourlyLi.textContent = tokyo.totalCookiesPerHourArray[i];
-  tokyoDiv.append(hourlyLi);
-}
-// create 'total' <li>
-var totalTokyoCookies = document.createElement('li');
-totalTokyoCookies.textContent = `Total: ${tokyo.totalDailyCookies}`;
-tokyoDiv.append(totalTokyoCookies);
-
-
-
-
-// dubai below:
-// create array
-for (var hour = 0; hour < operatingHours.length; hour++) {
-  var hourlyTotalCookies = dubai.avgCookiesPerSale * dubai.randomHourlyCustomers();
-  hourlyTotalCookies = Math.ceil(hourlyTotalCookies);
-  dubai.totalCookiesPerHourArray.push(`${operatingHours[hour]}: ${hourlyTotalCookies}`);
-  dubai.totalDailyCookies += hourlyTotalCookies;
-}
-// create <li>s via dom
-var dubaiDiv = document.getElementById('dubai');
-for (var i = 0; i < dubai.totalCookiesPerHourArray.length; i++) {
-  var hourlyLi = document.createElement('li');
-  hourlyLi.textContent = dubai.totalCookiesPerHourArray[i];
-  dubaiDiv.append(hourlyLi);
-}
-// create 'total' <li>
-var totalDubaiCookies = document.createElement('li');
-totalDubaiCookies.textContent = `Total: ${dubai.totalDailyCookies}`;
-dubaiDiv.append(totalDubaiCookies);
-
-
-
-// paris below:
-// create array
-for (var hour = 0; hour < operatingHours.length; hour++) {
-  var hourlyTotalCookies = paris.avgCookiesPerSale * paris.randomHourlyCustomers();
-  hourlyTotalCookies = Math.ceil(hourlyTotalCookies);
-  paris.totalCookiesPerHourArray.push(`${operatingHours[hour]}: ${hourlyTotalCookies}`);
-  paris.totalDailyCookies += hourlyTotalCookies;
-}
-// create <li>s via dom
-var parisDiv = document.getElementById('paris');
-for (var i = 0; i < paris.totalCookiesPerHourArray.length; i++) {
-  var hourlyLi = document.createElement('li');
-  hourlyLi.textContent = paris.totalCookiesPerHourArray[i];
-  parisDiv.append(hourlyLi);
-}
-// create 'total' <li>
-var totalParisCookies = document.createElement('li');
-totalParisCookies.textContent = `Total: ${paris.totalDailyCookies}`;
-parisDiv.append(totalParisCookies);
-
-
-
-// lima below:
-// create array
-for (var hour = 0; hour < operatingHours.length; hour++) {
-  var hourlyTotalCookies = lima.avgCookiesPerSale * lima.randomHourlyCustomers();
-  hourlyTotalCookies = Math.ceil(hourlyTotalCookies);
-  lima.totalCookiesPerHourArray.push(`${operatingHours[hour]}: ${hourlyTotalCookies}`);
-  lima.totalDailyCookies += hourlyTotalCookies;
-}
-// create <li>s via dom
-var limaDiv = document.getElementById('lima');
-for (var i = 0; i < lima.totalCookiesPerHourArray.length; i++) {
-  var hourlyLi = document.createElement('li');
-  hourlyLi.textContent = lima.totalCookiesPerHourArray[i];
-  limaDiv.append(hourlyLi);
-}
-// create 'total' <li>
-var totalLimaCookies = document.createElement('li');
-totalLimaCookies.textContent = `Total: ${lima.totalDailyCookies}`;
-limaDiv.append(totalLimaCookies);
-*/
