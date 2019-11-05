6d740d88135caec55eb894a0b1f9e4ed37b0105b
laura antonius <laurastolen@gmail.com>
2019-11-04 17:01:49 -0800

    Seattle object completed, DOM display completed for Seattle

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 'use strict';
 
-var operatingHours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7];
+// each item in operatinghours array is a string
+var operatingHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+// each item in the locations array is an object
+var locations = [seattle, tokyo, dubai, paris, lima];
 
 var seattle = {
+  name: 'Seattle',
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
   avgCookiesPerSale: 6.3,
@@ -12,72 +17,93 @@ var seattle = {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   },
   totalCookiesPerHourArray: [],
+  totalDailyCookies: 0
 }
-
-// console.log(seattle.avgCookiesPerSale);
-// console.log(seattle.randomHourlyCustomers());
-
-/* loop through operatinghours array, 
-call the randomhourlycustomers method,
-that random number times avgcookiespersale = total cookies per hour
-store all those hourlytotals in another array
-display the hourlytotals array, each index in a <li>
-*/
-
-for (var hour = 0; hour < operatingHours.length; hour++) {
-  var hourlyTotalCookies = seattle.avgCookiesPerSale * seattle.randomHourlyCustomers();
-  hourlyTotalCookies = Math.ceil(hourlyTotalCookies);
-  seattle.totalCookiesPerHourArray.push(hourlyTotalCookies);
-  console.log(`At ${operatingHours[hour]}: ${hourlyTotalCookies}`);
-}
-console.log(seattle.totalCookiesPerHourArray);
-
-
 
 var tokyo = {
+  name: 'tokyo',
   minHourlyCustomers: 3,
   maxHourlyCustomers: 24,
   avgCookiesPerSale: 1.2,
-  randomHourlyCustomers: function () {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    };
+  randomHourlyCustomers: function (min, max) {
+    min = this.minHourlyCustomers;
+    max = this.maxHourlyCustomers;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   },
-
+  totalCookiesPerHourArray: [],
 }
 
 var dubai = {
+  name: 'dubai',
   minHourlyCustomers: 11,
   maxHourlyCustomers: 38,
   avgCookiesPerSale: 3.7,
-  randomHourlyCustomers: function () {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    };
+  randomHourlyCustomers: function (min, max) {
+    min = this.minHourlyCustomers;
+    max = this.maxHourlyCustomers;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   },
-
+  totalCookiesPerHourArray: [],
 }
 
 var paris = {
+  name: 'paris',
   minHourlyCustomers: 20,
   maxHourlyCustomers: 38,
   avgCookiesPerSale: 2.3,
-  randomHourlyCustomers: function () {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    };
+  randomHourlyCustomers: function (min, max) {
+    min = this.minHourlyCustomers;
+    max = this.maxHourlyCustomers;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   },
-
+  totalCookiesPerHourArray: [],
 }
 
 var lima = {
+  name: 'lima',
   minHourlyCustomers: 2,
   maxHourlyCustomers: 16,
   avgCookiesPerSale: 4.6,
-  randomHourlyCustomers: function () {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    };
+  randomHourlyCustomers: function (min, max) {
+    min = this.minHourlyCustomers;
+    max = this.maxHourlyCustomers;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   },
-
+  totalCookiesPerHourArray: [],
 }
+
+/*
+for each location: (ie, loop through the locations array?)
+  loop through operatinghours array,
+  call the randomhourlycustomers method,
+  that random number times avgcookiespersale = total cookies per hour
+  store all those hourlytotals in another array
+  display the hourlytotals array, each index in a <li>
+*/
+
+for (var hour = 0; hour < operatingHours.length; hour++) {
+
+  var hourlyTotalCookies = seattle.avgCookiesPerSale * seattle.randomHourlyCustomers();
+  hourlyTotalCookies = Math.ceil(hourlyTotalCookies);
+  seattle.totalCookiesPerHourArray.push(`${operatingHours[hour]}: ${hourlyTotalCookies}`);
+  seattle.totalDailyCookies += hourlyTotalCookies;
+}
+
+/* to put the text in <li>s
+1 get a reference in the dom
+2 create elements we need to append to the dom
+3 set their contents
+4 add your elements to the dom
+*/
+
+var totalCookiesPerHourArray = [];
+var seattleDiv = document.getElementById('seattle');
+for (var i = 0; i < seattle.totalCookiesPerHourArray.length; i++) {
+  var li = document.createElement('li');
+  li.textContent = seattle.totalCookiesPerHourArray[i];
+  seattleDiv.append(li);
+}
+var totalSeattleCookies = document.createElement('li');
+totalSeattleCookies.textContent = `Total: ${seattle.totalDailyCookies}`;
+seattleDiv.append(totalSeattleCookies);
+// var operatingHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
