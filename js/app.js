20b43f7411348d3dd7332ade19845cffde7bdd0e
laura antonius <laurastolen@gmail.com>
2019-11-05 16:04:31 -0800

    functioning header

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const OPERATINGHOURS = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+const OPERATINGHOURS = ['', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 // cookiestore constructor:
 function CookieStore(storeLocation, minHourlyCust, maxHourlyCust, avgSale) {
@@ -18,17 +18,18 @@ function CookieStore(storeLocation, minHourlyCust, maxHourlyCust, avgSale) {
     }
   // method to calculate cookie sales per hour, stored in an array:
   this.calculateHourlySales = function () {
-    for (var hourIndex = 0; hourIndex < OPERATINGHOURS.length; hourIndex++) {
+    for (var hourIndex = 0; hourIndex < OPERATINGHOURS.length - 1; hourIndex++) {
       var customers = this.randHourlyCust();
       this.hourlySales.push(Math.ceil(this.avgSale * customers));
       this.dailyTotalCookies += this.hourlySales[hourIndex];
     }
   }
-
-  // render method
+  // render method:
   this.render = function (domReference) {
     var tr = document.createElement('tr');
-
+    var nameTd = document.createElement('td');
+    nameTd.textContent = this.storeLocation;
+    tr.append(nameTd);
     for (var i = 0; i < this.hourlySales.length; i++) {
       var td = document.createElement('td');
       td.textContent = this.hourlySales[i];
@@ -37,20 +38,12 @@ function CookieStore(storeLocation, minHourlyCust, maxHourlyCust, avgSale) {
     var dailyTotalCookies = document.createElement('td');
     dailyTotalCookies.textContent = `Total: ${this.dailyTotalCookies}`;
     tr.append(dailyTotalCookies);
-
     table.append(tr);
   }
-
-
-  // method to render operating hours array as <th>s:
-
-
 }
 
+// this creates the dom reference:
 var table = document.getElementById('sales-table');
-
-
-
 
 // creating new instances for each location:
 var seattle = new CookieStore('Seattle', 23, 65, 6.3);
@@ -61,10 +54,48 @@ var lima = new CookieStore('Lima', 2, 16, 4.6);
 
 var currentLocations = [seattle, tokyo, dubai, paris, lima];
 
+
+// render footer with totals for each hour among all locations:
+// add a td for 'totals' box
+// iterate over the operatinghours array
+// for each index of hour array, sum all first indices of hourly sales arrays
+// add a td for sum of all totals, outside loop
+// function renderFooter(domReference) {
+//   var tableFooter = document.createElement('tr');
+//   var totalsTd = document.createElement('td');
+//   totalsTd.textContent = 'Totals';
+//   // tableFooter.append(totalsTd);
+//   for (var hourlyTotalIndex = 0; hourlyTotalIndex < OPERATINGHOURS.length; hourlyTotalIndex++) {
+//     var hourlyTotal = 0;
+//     var currentHourTotal = document.createElement('td');
+//     for (var storeLocation = 0; storeLocation < currentLocations.length; storeLocation++){
+//       hourlyTotal += currentLocations[storeLocation];
+//     }
+//     hourlyTotal
+//   }
+//   var totalOfLocationTotals = document.createElement('td');
+//   totalOfLocationTotals.textContent = ;
+// }
+
+
+// render header:
+function renderHeader(domReference) {
+  var tableHeader = document.createElement('tr');
+  var hourlyHeaders = document.createElement('th');
+  for (var hourIndex = 0; hourIndex < OPERATINGHOURS.length; hourIndex++) {
+    var headers = document.createElement('th');
+    headers.textContent = OPERATINGHOURS[hourIndex];
+    tableHeader.append(headers);
+  }
+  table.append(tableHeader);
+}
+renderHeader(table);
+
+
+// renders table of all locations:
 for (var locationIndex = 0; locationIndex < currentLocations.length; locationIndex++) {
   var currentCity = currentLocations[locationIndex];
   currentCity.randHourlyCust();
   currentCity.calculateHourlySales();
   currentCity.render(table);
 }
-
